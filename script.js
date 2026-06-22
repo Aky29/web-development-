61903be6b998353c1fe6bf3849ebaf9804151fb4
Aky29 <me.anishkumaryadav@gmail.com>
2026-06-22 20:27:10 +0530

    enhanced

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-const slidervalue = document.getElementById("age");
-const agevalue = document.getElementById('agevalue');
-const submit = document.getElementById('submit')
-const button = document.querySelectorAll('.rbtn');
-console.log(button);
-function updatedata(){
-    agevalue.textContent = slidervalue.value;
-    let textsize = slidervalue.value +10;
-    agevalue.style.fontSize = slidervalue.value +'px';
-}
-function colorchange(btn){
- if (btn.checked) document.body.style.backgroundColor = btn.value;
-}
+    const slider = document.getElementById('age');
+    const output = document.getElementById('agevalue');
 
-slidervalue.addEventListener('input',updatedata);
-updatedata();
-
-button.forEach(radio => radio.addEventListener('change',() => colorchange(radio)));
+    // Update display when slider value changes
+    slider.addEventListener('input', function () {
+        output.textContent = this.value;
+    });
